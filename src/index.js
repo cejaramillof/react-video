5961375427b5960eaa99efdbcbc2f87e24434fe5
Carlos Jaramillo <Carlos.Jaramillo@blackboard.com>
2020-03-22 10:11:54 -0500

    add compose to debug redux in browser

--- src/index.js
+++ src/index.js
@@ -172,9 +172,9 @@ const initialState = {
   ]
 };
 
-// const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-// const store = createStore(reducer, initialState, composeEnhancers());
-const store = createStore(reducer, initialState);
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(reducer, initialState, composeEnhancers());
+// const store = createStore(reducer, initialState);
 
 ReactDOM.render(
   <Provider store={store}>
