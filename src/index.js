e4b30763b43a0090d67e4cc4641838f480b43d4a
Carlos Jaramillo <Carlos.Jaramillo@blackboard.com>
2020-03-26 16:39:03 -0500

    specificy createStore

--- src/index.js
+++ src/index.js
@@ -174,7 +174,7 @@ const initialState = {
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(reducer, initialState, composeEnhancers());
-// const store = createStore(reducer, initialState);
+// const store = createStore(allReducers, initialState);
 
 ReactDOM.render(
   <Provider store={store}>
